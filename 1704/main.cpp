f87614ba1e93dbc648b77712b60d2576678e33a0
Bruno Cesar <brunokuati@gmail.com>
2015-11-29 17:21:27 +0000

    1704

---
+++ 1704/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+  
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector< pii > vii;
+
+const int MAX = 100;
+
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define pq priority_queue
+#define IOFAST() ios_base::sync_with_stdio(0);cin.tie(0)
+#define EPS 1e-7
+#define PI  3.1415926535897932384626433832795028841971693993
+
+int main() {
+    
+    IOFAST();
+    
+    return ( 0 );
+}
